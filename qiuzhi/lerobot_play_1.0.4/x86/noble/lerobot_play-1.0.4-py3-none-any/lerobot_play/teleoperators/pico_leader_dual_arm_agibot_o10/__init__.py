27b4d6cce1608d3dce2ccd8b392d89c287ac6a3f
heliang pu <2303935680@qq.com>
2026-04-20 17:04:34 +0800

    feat: add dual-arm leader config class

---
+++ qiuzhi/lerobot_play_1.0.4/x86/noble/lerobot_play-1.0.4-py3-none-any/lerobot_play/teleoperators/pico_leader_dual_arm_agibot_o10/__init__.py
@@ -0,0 +1,11 @@
+from .config_pico_leader_dual_arm_agibot_o10 import (
+    PicoLeaderDualArmAgibotO10Config,
+)
+from .pico_leader_dual_arm_agibot_o10 import (
+    PicoLeaderDualArmAgibotO10,
+)
+
+__all__ = [
+    "PicoLeaderDualArmAgibotO10Config",
+    "PicoLeaderDualArmAgibotO10",
+]
---
+++ qiuzhi/lerobot_play_1.0.4/x86/noble/lerobot_play-1.0.4-py3-none-any/lerobot_play/teleoperators/pico_leader_dual_arm_agibot_o10/config_pico_leader_dual_arm_agibot_o10.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field
+from typing import List
+
+from lerobot.cameras.configs import CameraConfig
+from lerobot.teleoperators.config import TeleoperatorConfig
+
+
+@TeleoperatorConfig.register_subclass("pico_leader_dual_arm_agibot_o10")
+@dataclass
+class PicoLeaderDualArmAgibotO10Config(TeleoperatorConfig):
+    vr_pose_port: int = 8000
+    vr_ctrl_port: int = 8001
+    vr_device: str = "pico_wrist"
+    enable_hand: bool = True
+    left: dict = field(default_factory=dict)
+    right: dict = field(default_factory=dict)
+    disable_torque_on_disconnect: bool = True
+    max_relative_target: List[float] = field(default_factory=lambda: [0.1, 0.1])
+    cameras: dict[str, CameraConfig] = field(default_factory=dict)
+    use_degrees: bool = False
+    arm_joints_num: int = 7
+    serial_freq: int = 500
